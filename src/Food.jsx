a294997e69060eb8d45f428bad07a5b3d320620d
luthfishaj11@gmail.com <luthfishaj11@gmail.com>
2024-04-12 15:11:00 +0530

    Beginning tutorials

---
+++ src/Food.jsx
@@ -0,0 +1,13 @@
+
+function Food() {
+    const food1 = "Burger"
+    const food2 = "Sandwich"
+    return(
+        <ul>
+            <li>Pizza</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    );
+}
+export default Food
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return(
+        <footer>
+            <h3>&copy; BROCODE {new Date().getFullYear()}</h3>
+        </footer>
+    );
+}
+
+export default Footer
